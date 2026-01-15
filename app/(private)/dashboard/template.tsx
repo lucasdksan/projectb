3f88ed3d7672f43b90d0edb27e6abaea3204f78e
lucas <lucas.leoncio.silva@gmail.com>
2026-01-15 00:37:54 -0300

    Adição  do scroll no conteudo principal da dashboard

--- app/(private)/dashboard/template.tsx
+++ app/(private)/dashboard/template.tsx
@@ -17,9 +17,9 @@ export default async function DashboardTemplate({
       <main className="w-full h-screen lg:flex lg:flex-row">
           <NavigationMenu email={user?.email ?? ""} name={user?.name ?? ""}  />
           <SetProfileData email={user.email} name={user.name} />
-          <div className="w-full h-full flex flex-col">
+          <div className="flex-1 flex flex-col h-full w-full relative overflow-hidden">
             <DashboardHeader />
-            <section className="flex-1">
+            <section className="flex-1 overflow-y-auto">
                 { children }
             </section>
           </div>

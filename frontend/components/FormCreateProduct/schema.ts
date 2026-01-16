c7dd8677b6c1a8d6ca6793949e8031a2a843c587
lucas <lucas.leoncio.silva@gmail.com>
2026-01-16 00:26:04 -0300

    Mellhoria no schema

--- frontend/components/FormCreateProduct/schema.ts
+++ frontend/components/FormCreateProduct/schema.ts
@@ -1,16 +1,16 @@
 import z from "zod";
 
 export const addProductSchema = z.object({
-    name: z.string(),
-    description: z.string().optional(),
-    price: z.number().int().positive(),
-    stock: z.number().int().nonnegative(),
+    name: z.string().min(1, "Nome é obrigatório"),
+    description: z.string().optional().nullable(),
+    price: z.number().int().positive("Preço é obrigatório"),
+    stock: z.number().int().nonnegative("Estoque é obrigatório"),
     isActive: z.boolean(),
-    category: z.string(),
+    category: z.string().min(1, "Categoria é obrigatório"),
     storeId: z.number(),
     attributes: z.array(z.object({
-        kindof: z.string(),
-        value: z.string(),
+        kindof: z.string().min(1, "Tipo é obrigatório"),
+        value: z.string().min(1, "Valor é obrigatório"),
     })).optional(),
 });
 

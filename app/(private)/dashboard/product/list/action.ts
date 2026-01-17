c2584d7a4922dc3369f22c8117aa68a869b1b7d5
lucas <lucas.leoncio.silva@gmail.com>
2026-01-17 02:03:42 -0300

    Action para paginação

--- app/(private)/dashboard/product/list/action.ts
+++ app/(private)/dashboard/product/list/action.ts
@@ -1,10 +1,39 @@
 "use server";
 
+import { ProductService } from "@/backend/modules/product/product.service";
+import { paginationProductsSchema } from "@/backend/modules/product/product.types";
 import { StoreService } from "@/backend/modules/store/store.service";
 import { AppError } from "@/backend/shared/errors/app-error";
 
-export async function listProductsAction(userId: string){
+export async function paginationProductsAction(storeId: number, page: number, limit: number){
+    try {
+        const parsed = paginationProductsSchema.safeParse({ page, limit, storeId });
 
+        if (!parsed.success) {
+            return {
+                success: false,
+                errors: parsed.error.flatten().fieldErrors,
+                products: null,
+                message: "Dados inválidos",
+            };
+        }
+
+        const products = await ProductService.paginationByStoreId(parsed.data.storeId, parsed.data.page, parsed.data.limit);
+
+        return {
+            success: true,
+            products,
+            errors: null,
+            message: "Produtos encontrados com sucesso",
+        };
+    } catch (error) {
+        return {
+            success: false,
+            products: null,
+            errors: error instanceof AppError ? error.details : null,
+            message: error instanceof AppError ? error.message : "Erro ao buscar produtos",
+        };
+    }
 }
 
 export async function getStoreAction(userId: string){

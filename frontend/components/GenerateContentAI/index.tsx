5d67e71fd44ba74789d28c460b7ab577107847b9
lucas <lucas.leoncio.silva@gmail.com>
2026-01-19 15:13:13 -0300

    Geraçção de IA para plataformas

--- frontend/components/GenerateContentAI/index.tsx
+++ frontend/components/GenerateContentAI/index.tsx
@@ -1,6 +1,8 @@
+import CardIA from "./CardIA";
+
 export default function GenerateContentAI() {
     return (
-        <div className="bg-surface rounded-xl p-6 shadow-sm border border-[#e5e8e5] sticky top-24">
+        <div className="bg-surface rounded-xl p-6 shadow-sm border border-[#e5e8e5] sticky top-24 max-h-[582px]">
             <div className="flex items-center gap-3 mb-2">
                 <div className="size-10 rounded-full bg-primary/20 flex items-center justify-center text-primary">
                     <span className="material-symbols-outlined">Chat</span>
@@ -9,7 +11,9 @@ export default function GenerateContentAI() {
             </div>
             <p className="text-sm text-text-secondary mb-6 pl-11">Crie textos persuasivos para seus canais de venda em segundos.</p>
             <div className="flex flex-col gap-4">
-
+                <CardIA text="Legenda para post com hashtags virais." title="Instagram" btnText="Gerar Post" colorIcon="bg-gradient-to-tr from-purple-600 via-pink-500 to-orange-400" />
+                <CardIA text="Mensagem curta para lista de contatos." title="WhatsApp" btnText="Gerar Mensagem" colorIcon="bg-[#25D366]" />
+                <CardIA text="Descrição técnica otimizada para SEO." title="Shopee" btnText="Otimizar Texto" colorIcon="bg-[#EE4D2D]" />
             </div>
         </div>
     );

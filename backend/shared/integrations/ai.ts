dffd44728dc0e468a59aac518c379a8c0dddc997
lucas <lucas.leoncio.silva@gmail.com>
2026-01-21 01:39:04 -0300

    suporte para gerar informações apartir de uma imagem e prompt

--- backend/shared/integrations/ai.ts
+++ backend/shared/integrations/ai.ts
@@ -9,7 +9,28 @@ export const generativeAIUtils = {
     singlePrompt: async function (prompt: string) {
         const _this = this;
         const result = await _this.model.generateContent(prompt);
-        
+
+        return {
+            data: result.response.text(),
+        }
+    },
+
+    singlePromptWithImage: async function (prompt: string, file: Blob) {
+        const _this = this;
+
+        const imageBytes = await file.arrayBuffer();
+        const imgBase64 = Buffer.from(imageBytes).toString("base64");
+
+        const result = await _this.model.generateContent([
+            { text: prompt },
+            {
+                inlineData: {
+                    mimeType: file.type,
+                    data: imgBase64
+                }
+            }
+        ]);
+
         return {
             data: result.response.text(),
         }

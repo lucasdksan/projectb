e5c36c4b0fa4f78f5690279ea1d1c3d276afb30a
lucas <lucas.leoncio.silva@gmail.com>
2026-01-23 13:42:07 -0300

    Implementação do form e da apresentação da config

--- app/(private)/dashboard/store/page.tsx
+++ app/(private)/dashboard/store/page.tsx
@@ -1,7 +1,10 @@
 import { StoreService } from "@/backend/modules/store/store.service";
+import FormAddInstagramConfig from "@/frontend/components/FormAddInstagramConfig";
 import FormUpdateStore from "@/frontend/components/FormUpdateStore";
 import { getCurrentUser } from "@/libs/auth";
 import { redirect } from "next/navigation";
+import { getInstagramConfigAction } from "./action";
+import WhiteCard from "@/frontend/components/WhiteCard";
 
 export default async function DashboardStore(){
     const user = await getCurrentUser();
@@ -9,6 +12,7 @@ export default async function DashboardStore(){
     if (!user) redirect("/login");
     
     const store = await StoreService.findByUserId(`${user.sub}`);
+    const instagramConfig = await getInstagramConfigAction(store?.id ?? 0);
 
     return(
         <div className="w-full h-full p-6 flex flex-col gap-3">
@@ -23,6 +27,28 @@ export default async function DashboardStore(){
                     </div>
                     { store && (
                         <FormUpdateStore {...store} id={`${store.id}`} />
+                    ) }
+                </div>
+                <div className={`
+                    bg-surface rounded-xl p-6 shadow-sm border border-[#e5e7eb]
+                    ${instagramConfig.success ? "max-h-[250px]" : ""}
+                    `}>
+                    <div className="flex items-center gap-2 mb-6 pb-4 border-b border-[#f0f4f0]">
+                        <span className="text-text-main text-lg font-bold">Configuração do Instagram</span>
+                    </div>
+                    { store && !instagramConfig.success && <FormAddInstagramConfig storeId={store.id} /> }
+                    { store && instagramConfig.success && (
+                        <WhiteCard>
+                            <div className="flex items-center gap-2">
+                                  <span>A loja já possui configuração do Instagram</span>                             
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span>ID do Usuário: {instagramConfig.instagramConfig?.userInstagramId}</span>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span>ID da Loja: {instagramConfig.instagramConfig?.storeId}</span>
+                            </div>
+                        </WhiteCard>
                     ) }
                 </div>
             </div>

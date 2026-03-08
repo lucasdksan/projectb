79afc9f97951320d76510116a426cce486547545
lucas <lucas.leoncio.silva@gmail.com>
2026-03-07 22:53:32 -0300

    Melhorando a tipagem

--- frontend/components/StoreArea/storearea.model.ts
+++ frontend/components/StoreArea/storearea.model.ts
@@ -1,15 +1,17 @@
+import type { ConfigStoreModel } from "../ConfigStoreModal/configstoremodal.model";
+
+export interface StoreData {
+    name: string;
+    email: string;
+    number: string;
+    description: string;
+    typeMarket: string;
+    config?: ConfigStoreModel | null;
+}
+
 export interface StoreAreaProps {
-    store: {
-        name: string;
-        email: string;
-        number: string;
-        description: string;
-        typeMarket: string;
-        primaryColor?: string;
-        secondaryColor?: string;
-        logoUrl?: string;
-    } | undefined | null;
-}   
+    store: StoreData | undefined | null;
+}
 
 export type StoreFormState = {
     name: string;

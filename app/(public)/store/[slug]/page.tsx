609c02b9f52a9092425372fd3a2911d304b51dca
lucas <lucas.leoncio.silva@gmail.com>
2026-03-07 23:52:08 -0300

    Página do produto

--- app/(public)/store/[slug]/page.tsx
+++ app/(public)/store/[slug]/page.tsx
@@ -1,13 +1,32 @@
+import { getStoreBySlugAction } from "./getstorebyslug.action";
+
 export default async function StorePage({
     params,
 }: {
     params: Promise<{ slug: string }>;
 }) {
     const { slug } = await params;
+    const result = await getStoreBySlugAction(slug);
+    const { success, data } = result;
+
+    if (!success || !data) {
+        return (
+            <div>
+                <h1>Erro ao buscar loja</h1>
+            </div>
+        )
+    }
+
+    console.log(data);
     
     return (
-        <div>
-            <h1>Store</h1>
+        <div className="w-full h-full flex flex-col">
+            <div className="w-full h-16" style={{ backgroundColor: data.store.config?.primaryColor }}>
+
+            </div>
+            <div className="w-full h-screen mx-auto min-h-screen flex flex-col bg-white flex-1">
+
+            </div>
         </div>
     )
 }

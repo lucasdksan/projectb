c93006c54aa5045b60ff0cde84c5ff21b6b68db9
lucas <lucas.leoncio.silva@gmail.com>
2026-03-16 10:49:40 -0300

    atualizando as rotas

--- proxy.ts
+++ proxy.ts
@@ -24,6 +24,7 @@ const publicRoutes = [
     { path: "/store/:slug/checkout", whenAuthenticated: "next" },
     { path: "/store/:slug/cart", whenAuthenticated: "next" },
     { path: "/store/:slug/orderPlaced", whenAuthenticated: "next" },
+    { path: "/store/:slug/orderConfirmed", whenAuthenticated: "next" },
     { path: "/", whenAuthenticated: "next" },
 ] as const;
 
